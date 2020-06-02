0d4aec5fbb70883ba3b94bd4d0dd00344e053472
john.costa <john.costa@usask.ca>
2020-06-02 08:10:13 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
+############################################################################################
+##
+## R Programming - Week 3 - Assignment 1
+## J Costa
+##
+############################################################################################
 
-## Write a short comment describing this function
+############################################################################################
+# makeCacheMatrix - Function to invert matrix and cache in global memory
+############################################################################################
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # clear global vars
+  mtrx <<- x 
+  imtrx <<- NULL
+  
+  #define function "set" and sets a global vairable x, and resets global variable m
+  setMatrix <- function(y) { 
+    mtrx <<- y
+    imtrx <<- NULL
+  }
+  
+  # defines function "getMatrix" as a returning global variable mtrx
+  getMatrix <- function() mtrx
+  
+  # defines function "invMatrix" inverts mtrx and returns global variable imtrx
+  invMatrix <- function() imtrx <<- solve(mtrx)
+  
+  # defines function "getMatrix" as a returning global variable mtrx
+  getiMatrix <- function() imtrx
+  
+  # defines function "clrMatrix" - clear contents of imtrx
+  clriMatrix <- function() imtrx <<- NULL
+  
+  #creates and returns(?) a list of functions
+  list(
+        setMatrix = setMatrix,
+        getMatrix = getMatrix,
+        invMatrix = invMatrix,
+        getiMatrix = getiMatrix,
+        clriMatrix = clriMatrix
+      )
 }
 
+######################################################################
+# cacheMatrix - Function to call makeCacheMatrix invert matrix or use cached matrix
+######################################################################
 
-## Write a short comment describing this function
+cacheMatrix <- function(x, ...) {
+  
+  # Calls list with function get inverted matrix
+  im <- x$getiMatrix()
+  
+  # Checks if global variable is null
+  if(!is.null(im)) {
+    message("Cache is availble... using cache")
+    # The inverse of the matrix already exists - no need to calculate; return the inverted matrix
+    return(im)
+  }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Otherwise, calculate the Inverted matrix  
+  # Data is now avaialble (either from above or b/c it was there already), and return the data
+  message("\nCache not available... generating inverted matrix\n")
+  x$invMatrix()
+  im <- x$getiMatrix()
+  
+  return(im)
 }
+######################################################################
